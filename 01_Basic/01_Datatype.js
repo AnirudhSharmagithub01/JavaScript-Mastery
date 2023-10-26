685cfaeb29a47810e42ee6bf4403445e41314766
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2023-10-26 16:20:28 +0530

    Datatypes done

---
+++ 01_Basic/01_Datatype.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("hello")  // we are using nodejs, not brower this is works on browser console
+
+// code readability should be high
+
+let name = "Smith"
+let age = 35
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true / false
+// null => standalone value /* */
+// undefined => 
+// symbol => when we talk about unique thinks
+
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
